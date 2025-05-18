7b744cc861988645594e8cf2dbebc2c10bb4e634
Rodrigo Weller Zarrabal <87690107+zarry0@users.noreply.github.com>
2025-05-18 15:33:10 -0600

    Update setupLangs.sh
    Updated - java to v17
    Added - ruby v3.3.5

--- setup/setupLangs.sh
+++ setup/setupLangs.sh
@@ -13,13 +13,23 @@ if command -v asdf &> /dev/null; then
     echo "asdf is already installed."
 else
     echo "asdf is not installed."
-    echo "Installing asdf prerequisites..."
-    brew install coreutils curl git
-    check_installation_error
+    # echo "Installing asdf prerequisites..."
+    # brew install coreutils curl git
+    # check_installation_error
     echo "Installing asdf..."
     brew install asdf
     check_installation_error
-    echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+    # echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+    export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
+    echo "Adding asdf completions..."
+    mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+    asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
+    # append completions to fpath
+    fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+    # initialise completions with ZSH's compinit
+    autoload -Uz compinit && compinit
+    
 fi
 
 echo "Installing NodeJS"
@@ -32,4 +42,22 @@ asdf install python 3.10.0
 
 echo "Installing java"
 asdf plugin add java
-asdf install java temurin-23.0.1+11
+asdf install java temurin-17.0.14+7
+
+echo "Installing ruby"
+asdf plugin add ruby
+ruby_version=3.3.5
+if asdf install ruby ${ruby_version}; then
+    echo "Installed ruby ${ruby_version}"
+else
+    echo "Failed to install ruby ${ruby_version}. Trying again (forcing ruby-build update)."
+    echo "Installing libyaml with brew..."
+    if ! brew list libyaml &>/dev/null; then
+      brew install libyaml
+      check_installation_error
+    else
+      echo "libyaml already installed"
+    fi
+    echo "Installing ruby ${ruby_version} with asdf"
+    ASDF_RUBY_BUILD_VERSION=master asdf install ruby ${ruby_version}
+fi
